4b32f4263978ceae4fecf89df9c7faefa195bf25
raresmigea <raresini@yahoo.com>
2020-11-21 18:44:12 +0200

    added send message functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
@@ -49,6 +49,21 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+
+    setFormValue('');
+  };
 
   return (
     <>
@@ -56,19 +71,28 @@ function ChatRoom() {
         {messages &&
           messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
       </div>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e = setFormValue(e.target.value))}
+        />
+        <button type='submit'>send</button>
+      </form>
     </>
   );
-
-  function ChatMessage(props) {
-    const { text, uid } = props.message;
-
-    const messageClass = id === auth.currentUser.uid ? 'sent' : 'received';
-
-    return (
-      <div className={`message ${messageClass}`}>
-        <p>{text}</p>
-      </div>
-    );
-  }
 }
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt='no' />
+      <p>{text}</p>
+    </div>
+  );
+}
+
 export default App;
