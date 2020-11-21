047fb04d1ffcb08aa1a96339473acef1dd1c82c0
raresmigea <raresini@yahoo.com>
2020-11-21 18:34:38 +0200

    chatMessage function

--- src/App.js
+++ src/App.js
@@ -49,5 +49,26 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  );
+
+  function ChatMessage(props) {
+    const { text, uid } = props.message;
+
+    const messageClass = id === auth.currentUser.uid ? 'sent' : 'received';
+
+    return (
+      <div className={`message ${messageClass}`}>
+        <p>{text}</p>
+      </div>
+    );
+  }
 }
 export default App;
