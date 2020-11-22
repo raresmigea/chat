e84d503188ad4173028903e99da3e1748c8ed591
raresmigea <raresini@yahoo.com>
2020-11-22 20:05:33 +0200

    updated placeholder

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function ChatRoom() {
         <input
           value={formValue}
           onChange={(e) => setFormValue(e.target.value)}
-          placeholder='say something nice'
+          placeholder='write something...'
         />
 
         <button type='submit' disabled={!formValue}>
