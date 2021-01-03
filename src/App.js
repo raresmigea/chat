b4f7b1c05cc13f06cfadd53945199538a27988f2
raresmigea <raresini@yahoo.com>
2021-01-03 22:03:30 +0200

    removed icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -27,8 +28,12 @@ function App() {
 
   return (
     <div className='App'>
-      <header></header>
-      <section> {user ? <ChatRoom /> : <SignIn />}</section>
+      <header>
+        <h1>💬</h1>
+        <SignOut />
+      </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
 }
@@ -39,11 +44,26 @@ function SignIn() {
     auth.signInWithPopup(provider);
   };
 
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+  return (
+    <>
+      <button className='sign-in' onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
+    </>
+  );
 }
 
 function SignOut() {
-  return auth.currentUser && <button onClick={() => auth.signOut}></button>;
+  return (
+    auth.currentUser && (
+      <button className='sign-out' onClick={() => auth.signOut()}>
+        Sign Out
+      </button>
+    )
+  );
 }
 
 function ChatRoom() {
@@ -52,10 +72,12 @@ function ChatRoom() {
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
+
   const [formValue, setFormValue] = useState('');
 
   const sendMessage = async (e) => {
     e.preventDefault();
+
     const { uid, photoURL } = auth.currentUser;
 
     await messagesRef.add({
@@ -66,7 +88,6 @@ function ChatRoom() {
     });
 
     setFormValue('');
-
     dummy.current.scrollIntoView({ behavior: 'smooth' });
   };
 
@@ -76,8 +97,9 @@ function ChatRoom() {
         {messages &&
           messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
 
-        <div ref={dummy}></div>
+        <span ref={dummy}></span>
       </main>
+
       <form onSubmit={sendMessage}>
         <input
           value={formValue}
@@ -99,10 +121,17 @@ function ChatMessage(props) {
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL} alt='no' />
-      <p>{text}</p>
-    </div>
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'
+          }
+          alt={'nothing'}
+        />
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
 
